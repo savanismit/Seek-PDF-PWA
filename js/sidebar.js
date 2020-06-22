285f3632198d579fb898a48da5314f3a353a7d3d
savanismit <savanismit11@example.com>
2020-06-22 17:42:00 +0530

    Title added and index Page changed

--- js/sidebar.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    // SideNav Button Initialization
-    $(".button-collapse").sideNav2();
-      // SideNav Scrollbar Initialization
-      var sideNavScrollbar = document.querySelector('.custom-scrollbar');
-      var ps = new PerfectScrollbar(sideNavScrollbar);
-});
