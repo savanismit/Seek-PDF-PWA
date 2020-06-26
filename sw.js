a4636b5b4af4065bc382f4c1ae9c830e10588f96
savanismit <savanismit11@example.com>
2020-06-26 20:24:16 +0530

    all files added

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
-const staticCacheName = 'site-static-v1';
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
   '/',
   '/index.html',
+  '/js/app.js',
+  
+  '/sidebar.html',
+
   '/pages/about.html',
   '/pages/AI.html',
   '/pages/bigdata.html',
@@ -19,7 +24,7 @@ const assets = [
   '/pages/pandas.html',
   '/pages/python.html',
   '/pages/rcheetsheet.html',
-  '/sidebar.html',
+  '/pages/sidebar.html',
 
   '/js/sidebar.js',
   '/js/jquery-3.2.1.min.js',
@@ -38,13 +43,16 @@ const assets = [
   '/css/bootstrap4/bootstrap.min.js',
   '/css/bootstrap4/popper.js',
 
-  '/.vscode/setting.json',
-  
-  'https://fonts.googleapis.com/css?family=Lato:300,400,700',
+
+
+
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
+
 // install event
 self.addEventListener('install', evt => {
-  console.log("install");
+  //console.log('service worker installed');
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log('caching shell assets');
@@ -52,21 +60,24 @@ self.addEventListener('install', evt => {
     })
   );
 });
-const dynamicCacheName = 'site-dynamic-v1';
+
 // activate event
 self.addEventListener('activate', evt => {
-  console.log("acti");
+  //console.log('service worker activated');
   evt.waitUntil(
     caches.keys().then(keys => {
+      //console.log(keys);
       return Promise.all(keys
-        .filter(key =>  key !== dynamicCacheName)
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
         .map(key => caches.delete(key))
       );
     })
   );
 });
+
 // fetch event
 self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
       return cacheRes || fetch(evt.request).then(fetchRes => {
